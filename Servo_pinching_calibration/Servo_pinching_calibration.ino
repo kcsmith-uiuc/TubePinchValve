9c51694c55821cab17b57ba03524af964dbc6c05
kcsmith-uiuc <kcsmith@illinois.edu>
2025-05-06 15:51:01 -0500

    Update Servo_pinching_calibration.ino

--- Servo_pinching_calibration/Servo_pinching_calibration.ino
+++ Servo_pinching_calibration/Servo_pinching_calibration.ino
@@ -3,15 +3,18 @@ Servo s1;
 Servo s2;
 Servo s3;
 Servo s4;
-boolean purification=0;
-int initAngle = 96;
+boolean system_state=0; //Set the state of the system: 0=State 1; 1=State 2
+int initAngle = 96; //Specify the initial angle for all servos here
+int dly = 1000; //Delay time in ms between switching inlet and outlet valves
 
 void setup() {
-  // put your setup code here, to run once:
+//Assign each servo to an Arduino pin
 s1.attach(3);
 s2.attach(9);
 s3.attach(10);
 s4.attach(11);
+
+//Position each servo at the specified initial angle, initAngle
 s1.write(initAngle);
 s2.write(initAngle);
 s3.write(initAngle);
@@ -19,25 +22,25 @@ s4.write(initAngle);
 }
 
 void loop() {
-while (analogRead(A1)<251){ //Loop until triggered
+while (analogRead(A1)<251){ //Loop without switching, until triggered
   digitalWrite(13, LOW);
 }
 digitalWrite(13, LOW);
-purification=!purification;
-if (purification){
-  s1.write(75); //Move servo s4 to 75 degree
-  s2.write(118);  //Move servo s2 to 118 degree
-  delay(1000);   //Pausing time, ms
-  s3.write(120);  //Move servo s1 to 120 degree
-  s4.write(80); //Move servo s3 to 80 degree
-} else {
-  s1.write(118); //Move servo s4 to 118 degree
-  s2.write(75);  //Move servo s2 to 75 degree
-  delay(1000);   //Pausing time, ms
-  s3.write(82);  //Move servo s1 to 82 degree
-  s4.write(120); //Move servo s3 to 120 degree
-}
+system_state=!system_state; //Switch the system state
 
-// delay(1000); //Delay to avoid cycling multiple times on one trigger. Shorten if needed
-//   increase angle is cc, decrease is c.
+//The servo positions that are used for states 2 and 1 can be adjusted
+//by the user below to optimize the degree of tube pinching that occurs.  
+if (system_state){ //Switch system state 2 to state 1
+  s1.write(75); //Move servo s1 to 75 degrees
+  s2.write(118); //Move servo s2 to 118 degrees
+  delay(dly); //Pausing time, ms
+  s3.write(120); //Move servo s3 to 120 degrees
+  s4.write(80); //Move servo s4 to 80 degrees
+} else { //Switch system state 1 to state 2
+  s1.write(118); //Move servo s1 to 118 degrees
+  s2.write(75); //Move servo s2 to 75 degrees
+  delay(dly); //Pausing time, ms
+  s3.write(82); //Move servo s3 to 82 degrees
+  s4.write(120); //Move servo s4 to 120 degrees
+}
 }
